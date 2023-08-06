f8c87f26cbfe39b8f9fd659e361cda331e67cf8b
Pmaxion <p@maxion.tech>
2023-08-06 23:44:57 +0700

    text & logos

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ const navigation = [
 	// { name: "Contact", href: "/contact" },
 ];
 
+const campaignDescription = `เคล็ดลับ วิธีเอาตัวรอด ในโลกยุคใหม่! กับสังคมดิจิทัล หรือสื่อออนไลน์ต่างๆ พร้อมทำความเข้าใจ และจำลองสถานการณ์ของ พฤติกรรม ในเชิงลึก`;
+
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-white via-zinc-600/20 to-white">
@@ -30,14 +32,26 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl  text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			{/* <h1 className="z-10 text-4xl  text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 			MomickeyoM
-			</h1>
+			</h1> */}
+
+			{/* <div className="flex justify-center mt-8">
+				<img alt="Chapter-01.pdf" className="irnPqc" style={{ height: `10rem` }} src={`${`/esocial-manual.jpeg`}`} data-iml="91125.90000000037" data-atf="true"></img>
+			</div> */}
+
+			<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-3 ">
+			<img alt="Loading .." className="irnPqc" style={{ height: `10rem` }} src={`${`/eiei.jpg`}`} data-iml="91125.90000000037" data-atf="true"></img>
+			<img alt="Loading .." className="irnPqc" style={{ height: `10rem` }} src={`${`/msv.png`}`} data-iml="91125.90000000037" data-atf="true"></img>
+			<img alt="Loading .." className="irnPqc" style={{ height: `10rem` }} src={`${`/tmf.png`}`} data-iml="91125.90000000037" data-atf="true"></img>
+			
+				</div>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-white ">
-					Hi, my name is Ton Rattasapa Chu, I'm building serverless and open source
+					{campaignDescription}
+					{/* Hi, my name is Ton Rattasapa Chu, I'm building serverless and open source
 					solutions at{" "}
 					<Link
 						target="_blank"
@@ -56,7 +70,7 @@ export default function Home() {
 					>
 						unkey.dev
 					</Link>{" "}
-					at night.
+					at night. */}
 				</h2>
 			</div>
 		</div>
